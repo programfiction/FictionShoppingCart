47f8efe6490aa5e87ea82a197268dd10f718e86f
Ramakrushna Mishra <ram2mishra@gmail.com>
2023-02-14 14:55:21 +0530

    Added Dtos

--- api/Controllers/ProductsController.cs
+++ api/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using core.Entities;
 using core.Interfaces;
 using core.Specifications;
+using api.Dtos;
 
 namespace api.Controllers;
 
@@ -19,20 +20,44 @@ public class ProductsController : ControllerBase
         _brandRepo = brandRepo;
         _typeRepo = typeRepo;
     }
-    // [HttpGet]
-    // public async Task<ActionResult<Product>> GetProducts()
-    // {
-    //     var spec = new ProductsWithTypesAndBrandsSpecification();
-    //     var products = await _productRepo.ListAsync(spec);
-    //     return Ok(products);
-    // }
     [HttpGet]
-    public async Task<ActionResult<Product>> GetProducts() => Ok(await _productRepo.ListAsync(new ProductsWithTypesAndBrandsSpecification()));
+    public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
+    {
+        var spec = new ProductsWithTypesAndBrandsSpecification();
+        var products = await _productRepo.ListAsync(spec);
+
+        return Ok(products.Select(product => new ProductToReturnDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Description = product.Description,
+            PictureUrl = product.PictureUrl,
+            Price = product.Price,
+            ProductBrand = product.ProductBrand.Name,
+            ProductType = product.ProductType.Name,
+        }).ToList());
+    }
+    // [HttpGet]
+    // public async Task<ActionResult<Product>> GetProducts() => Ok(await _productRepo.ListAsync(new ProductsWithTypesAndBrandsSpecification()));
 
 
+    // [HttpGet("{id}")]
+    // public async Task<ActionResult<Product>> GetProduct(int id) => Ok(await _productRepo.GetEntityWithSpec(new ProductsWithTypesAndBrandsSpecification(id)));
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetProduct(int id) => Ok(await _productRepo.GetEntityWithSpec(new ProductsWithTypesAndBrandsSpecification(id)));
-
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+    {
+        var product = await _productRepo.GetEntityWithSpec(new ProductsWithTypesAndBrandsSpecification(id));
+        return new ProductToReturnDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Description = product.Description,
+            PictureUrl = product.PictureUrl,
+            Price = product.Price,
+            ProductBrand = product.ProductBrand.Name,
+            ProductType = product.ProductType.Name,
+        };
+    }
     [HttpGet("brands")]
     public async Task<ActionResult<ProductBrand>> GetProductBrands() => Ok(await _brandRepo.ListAsync(new ProductBrandsSpecification()));
 
